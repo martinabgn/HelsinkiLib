8450cbe1f44618510be1141af8ef92c2ca79d365
Howlynight <owl.git@aprilfam.me>
2025-01-24 13:11:57 +0200

    User input. Issue #2

--- small_search_engine.py
+++ small_search_engine.py
@@ -1,1 +1,11 @@
 # Here is the script we need to finish for Week 2
+
+#User input
+
+print("Welcome to our search engine!")
+print("To exit press enter")
+query = input("Please, write your query here: ")
+
+while query != "":
+
+    #Here goes the code to process the query
