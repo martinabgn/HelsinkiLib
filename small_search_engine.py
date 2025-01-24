1b2c2273c0058a2fa6e13ab42cf033c21b28b932
Howlynight <owl.git@aprilfam.me>
2025-01-24 13:26:55 +0200

    Working with wrong input. Error messages. Issue #4

--- small_search_engine.py
+++ small_search_engine.py
@@ -3,9 +3,24 @@
 #User input
 
 print("Welcome to our search engine!")
+print("You can use operators such as AND, OR, NOT in your query")
 print("To exit press enter")
 query = input("Please, write your query here: ")
 
 while query != "":
+    try
 
     #Here goes the code to process the query
+
+
+
+
+
+
+
+#Handling incorrect input
+except SyntaxError:
+    print("Please, enter your query in the format of: word OPERATOR word separated by spaces")
+
+except:
+    print("Sorry, something went wrong.")
